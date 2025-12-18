e7a9e5def3939de66257f2b19da3e5b2bcb8fb8c
kasmbafumoja <kaskas2008910@gmail.com>
2025-12-18 22:56:24 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 export const metadata = {
   title: "AI Chat Bot",
-  description: "ChatGPT & Gemini assistant"
+  description: "ChatGPT & Gemini Assistant"
 };
 
 export default function RootLayout({ children }) {
